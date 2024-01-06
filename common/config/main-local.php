aaa8aaa071685ba0c9b60b7e9f9a4fb74524c4dc
Andrii <sferaved@gmail.com>
2024-01-06 14:10:53 +0200

    2.980 android

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=admin_yii_2022',
+            'dsn' => 'mysql:host=localhost;dbname=admin_yii_2023',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
