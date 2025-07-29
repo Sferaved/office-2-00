5c8ea2564be8f07ab2e82180dcd44c7ecebc2b16
Andrii <sferaved@gmail.com>
2025-07-29 15:06:55 +0300

    helper

---
+++ build_and_push_OFFICE.bat
@@ -0,0 +1,22 @@
+@echo off
+
+chcp 65001 > nul
+
+cd /d "D:\OpenServer\domains\of2024"
+
+echo Building Docker image...
+docker build -t ghcr.io/andrey18051/office:1.0  .
+if %ERRORLEVEL% NEQ 0 (
+    echo Error building Docker image.
+    exit /b 1
+)
+echo Docker image built successfully.
+
+echo Pushing Docker image to GitHub...
+docker push ghcr.io/andrey18051/office:1.0
+if %ERRORLEVEL% NEQ 0 (
+    echo Error pushing Docker image.
+    exit /b 1
+)
+echo Docker image pushed to GitHub successfully.
+
